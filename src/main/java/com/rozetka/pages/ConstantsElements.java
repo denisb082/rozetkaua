52b3a33cc0bc7170da0381cf1b02b9e4a2d79313
denis <denis.beznisko@gmail.com>
2017-02-01 08:56:16 +0200

    jenkins test

--- src/main/java/com/rozetka/pages/ConstantsElements.java
+++ src/main/java/com/rozetka/pages/ConstantsElements.java
@@ -8,11 +8,9 @@ import com.rozetka.pages.WebElementsPage;
 public final class ConstantsElements {
 
     public final static String PHONES_TV_ELECTRONICS_LINK = "//a[@data-title='Телефоны, ТВ и электроника']";
-    public final static String PHONES_LINK = "Телефоны";
-    public final static String SMARTPHONES_LINK = "Смартфоны";
-/*    public final static String PAGE_TWO_LINK = "./*//*[@id='page2']/a";
-    public final static String PAGE_THREE_LINK = "./*//*[@id='page3']/a";*/
+    public final static String PHONES_LINK = ".//*[@id='menu_categories_left']/li[1]/h4/a";
+    public final static String SMARTPHONES_LINK = ".//*[@id='menu_categories_left']/li[1]/h4/a";
     public final static String TOP_SALE_XPATH = "//i[@class=\"g-tag g-tag-icon-middle-popularity sprite\" and @name=\"prices_active_element_original\"]/parent::*";
-    /*public final static String ID_XPATH = "./*//*[@id='goods_price_block_"+id+"'"*/
+
 
 }
--- src/main/java/com/rozetka/pages/WebElementsPage.java
+++ src/main/java/com/rozetka/pages/WebElementsPage.java
@@ -26,12 +26,6 @@ public class WebElementsPage extends BasePage {
     @FindBy(xpath = ConstantsElements.SMARTPHONES_LINK)
     private WebElement smartphonesLink;
 
-/*    @FindBy (xpath = ConstantsElements.PAGE_TWO_LINK)
-    private WebElement pageTwoLink;
-
-    @FindBy (xpath = ConstantsElements.PAGE_THREE_LINK)
-    private WebElement pageThreeLink;*/
-
     @FindBy(xpath = ConstantsElements.TOP_SALE_XPATH)
     private List<WebElement> list; // xpath fo top sale
 
@@ -77,6 +71,7 @@ public class WebElementsPage extends BasePage {
         for (int i = 1; i<4; i++){
             WebElement page = driver.findElement(By.xpath(".//*[@id='page"+ i +"']"));
             page.click();
+
         }
     }
 /*    public void clickOnSecondPage(){
